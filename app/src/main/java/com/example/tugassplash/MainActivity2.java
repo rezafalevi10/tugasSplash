89e714deaf71b43c585a7651313cd0b7cdc13bc4
reza <rezabudget@gmail.com>
2023-11-28 21:59:50 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugassplash/MainActivity2.java
@@ -0,0 +1,82 @@
+package com.example.tugassplash;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+    CardView hello;
+    CardView count;
+    CardView Sianida;
+    CardView twoactivity;
+    CardView setalarm;
+    CardView maps;
+    Uri geoLocation;
+    CardView Shopping;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        hello = (CardView)findViewById(R.id.to_hello);
+        count = (CardView)findViewById(R.id.to_count);
+        Sianida = (CardView)findViewById(R.id.to_sianida);
+        twoactivity = (CardView)findViewById(R.id.to_twoactivity);
+        setalarm = (CardView)findViewById(R.id.to_alarm);
+        maps = (CardView) findViewById(R.id.to_maps);
+        Shopping = (CardView) findViewById(R.id.to_shopping) ;
+
+        hello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Buka = new Intent(MainActivity2.this, helloactivity.class);
+                startActivity(Buka);
+            }
+        });
+        count.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Buka = new Intent(MainActivity2.this, countactivity.class);
+                startActivity(Buka);
+            }
+        });
+        Sianida.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Buka = new Intent(MainActivity2.this, sianidaActivity.class);
+                startActivity(Buka);
+            }
+        });
+        twoactivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Buka = new Intent(MainActivity2.this, Twoactivity.class);
+                startActivity(Buka);
+            }
+        });
+        setalarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Buka = new Intent(MainActivity2.this, Setalarm.class);
+                startActivity(Buka);
+            }
+        });
+        maps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(geoLocation);
+                if(intent.resolveActivity(getPackageManager()) != null){
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+}
